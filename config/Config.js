8651bec14df0e616587988d6e86cf50e17f28797
neha763 <nehaambekar1998@gmail.com>
2025-01-09 18:32:09 +0530

    Frontend files

--- config/Config.js
+++ config/Config.js
@@ -1,11 +1,11 @@
 const config = {
-    development: "http://localhost:8080", // Backend URL for local testing
-    production: "http://your-backend-domain.com/api/admin/register"
+    development: "http://3.110.134.175/:8080", // Backend URL for local testing
+    production: "/api"
 
 };
 
 // Automatically choose the correct environment
-const apiBaseURL = window.location.hostname === "localhost" ? config.development : config.production;
+const apiBaseURL = window.location.hostname === "http://51.21.80.154:8080/api/";  
 
 // Expose the API URL globally
 window.apiBaseURL = apiBaseURL;
